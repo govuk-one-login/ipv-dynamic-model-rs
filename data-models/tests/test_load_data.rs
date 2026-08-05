46fb6c24809c699dbd0e1f70edba3717e0239795
Daniel Mason <daniel.mason@dsit.gov.uk>
2026-08-05 11:52:00 +0100

    refactor: move the test data

--- data-models/tests/test_load_data.rs
+++ data-models/tests/test_load_data.rs
@@ -6,7 +6,7 @@ use std::fs::File;
 fn test_load_data() -> anyhow::Result<()> {
     println!("Loading test-data");
     let mut cris = Vec::new();
-    for document in yaml_serde::Deserializer::from_reader(File::open("./test-data/test.yaml")?) {
+    for document in yaml_serde::Deserializer::from_reader(File::open("../test-data/test.yaml")?) {
         cris.push(Cri::deserialize(document)?);
     }
     for cri in cris {
